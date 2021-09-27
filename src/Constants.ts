82caf2412441fd4ba451ce59ae5a8eddc1838bf6
Yogeswarr <yogeswarr.sathish@gmail.com>
2021-09-27 20:48:09 +0530

    added map support

--- src/Constants.ts
+++ src/Constants.ts
@@ -1,3 +1,2 @@
 export const MAIN_URL = "https://data.covid19india.org/v4/min/data.min.json"
 export const TIMESERIES_URL = "https://data.covid19india.org/v4/min/timeseries.min.json"
-
--- src/app/Components/home/home.component.ts
+++ src/app/Components/home/home.component.ts
@@ -1,22 +1,34 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { AppComponent } from 'src/app/app.component';
 import { SharedService } from 'src/app/shared.service';
-
+import { select, json, geoPath, geoMercator, scaleLinear, schemeBlues, schemeGreens } from 'd3'
+import { feature } from 'topojson-client'
+import * as d3 from 'd3';
+import { MAIN_URL } from 'src/Constants';
 
 @Component({
 	selector: 'app-home',
 	templateUrl: './home.component.html',
 	styleUrls: ['./home.component.scss']
 })
+
 export class HomeComponent implements OnInit {
+	
 	public stateData: any
-	constructor(private _appData: AppComponent, private themesService: SharedService) {}
+	public stateMap: any
+	public stateDataIn: any
+	constructor(private _appData: AppComponent, private themesService: SharedService, private http: HttpClient) {}
 	darkMode = false;
 	confirmedCases = 0
 	activeCases = 0
 	recovered = 0
 	deaths = 0
-
+	colors = scaleLinear<string>()
+		.domain([10000, 150000])
+		.range(schemeGreens[5])
+		
+	// colors = scaleLinear()
 	findConfirmedCases () {
 		let sum = 0
 		for (let i = 0; i < 37; i++) {
@@ -51,11 +63,13 @@ export class HomeComponent implements OnInit {
 		this.activeCases = this.confirmedCases - this.deaths - this.recovered - sum/2
 	}
 	ngOnInit() {
+		
 		this._appData.getData()
 			.subscribe(data => {
 				this.stateData = data
-				// console.log(this.stateData[Object.keys(this.stateData)[0]]["total"]["other"])
-				// console.log(Object.keys(this.stateData).length)
+				// console.log(this.stateData[Object.keys(this.stateData)[15]]["districts"])
+				this.stateDataIn = this.stateData[Object.keys(this.stateData)[15]]["districts"]
+				// console.log(Object.keys(this.stateData)[15])
 				// console.log(this.findTotalCases())
 
 				this.findConfirmedCases()
@@ -66,7 +80,66 @@ export class HomeComponent implements OnInit {
 			})
 
 		this.themesService.mode.subscribe(data => this.darkMode = data)
+		
 
+		let svg = select('#map')
+		
+		let g = svg.append('g');
+		// g.attr('class', 'map');
+
+		json('../../../assets/maps/karnataka.json')
+			.then((data: any) => {
+				let districts = feature(data, data.objects.districts)
+				this.stateMap = districts
+				let projection = geoMercator().fitSize([400, 400], districts);
+				let path = geoPath()
+					.projection(projection);
+				
+				let values: Array<number> = []
+				
+				// console.log(this.stateMap[Object.keys(this.stateMap)[1]][0]["properties"]["district"])
+				// console.log(this.stateMap[Object.keys(this.stateMap)[1]]);
+				
+				// json(MAIN_URL)
+				// 	.then((data: any) => {
+				// 		dataIN = data["KA"]["districts"]
+				// 		console.log(dataIN)
+
+				// 		Object.keys(dataIN).map(i => {
+				// 			// console.log(dataIN[i]["total"]["confirmed"])
+				// 			values.push(dataIN[i]["total"]["confirmed"])
+				// 		})
+				// 	})
+				g.selectAll('path')
+					.data(this.stateMap[Object.keys(this.stateMap)[1]])
+					.enter()
+					.append("path")
+					.attr("class", "district")
+					
+					.attr("stroke", "red")
+					.attr("d", (district: any) => path(district))
+					// .attr("fill", (d: any) => )
+					.attr("fill", (d: any) => {
+						let value;
+						// console.log(this.stateData["KA"])
+						Object.keys(this.stateData["KA"]["districts"]).find(i => {
+							if (i === d["properties"]["district"]) {
+								// console.log(i, this.stateData["KA"]["districts"][i]["total"]["confirmed"])
+								value = this.stateData["KA"]["districts"][i]["total"]["confirmed"]
+							}
+						})
+						if (value) {
+							return this.colors(value)
+						}
+						return "#ccc"
+					})
+					.append("title")
+					.text((i: any) => {
+						return ( i["properties"]["district"] + ':\n' + this.stateData["KA"]["districts"][i["properties"]["district"]]["total"]["confirmed"])
+					})
+
+					
+			})
 	}
 
 }
